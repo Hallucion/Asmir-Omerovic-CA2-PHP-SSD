8c3d792a0e4c0c34dd3d6f97687c09acf45f1c81
D00234895 <D00234895@student.dkit.ie>
2022-02-21 13:40:39 +0000

    tests for today updates later

--- pharmacy.sql
+++ pharmacy.sql
@@ -64,14 +64,15 @@ INSERT INTO `records` (`recordID`, `categoryID`, `name`, `price`, `image`) VALUE
 (2, 1, 'Some text', '15.00', '233012.jpg'),
 (3, 1, 'Some text', '18.00', '329484.jpg'),
 (4, 1, 'Some text', '10.00', '644055.jpg'),
-(5, 2, 'Some text', '16.00', '373465.jpg'),
-(6, 2, 'Some text', '19.00', '373989.jpg'),
-(7, 2, 'Some text', '12.00', '374104.jpg'),
-(8, 2, 'Some text', '10.00', '4733.jpg'),
-(9, 2, 'Some text', '15.00', '834551.jpg'),
-(10, 3, 'Some text', '13.00', '908783.jpg'),
-(11, 3, 'Some text', '17.00', '835545.jpg'),
-(12, 3, 'Some text', '19.00', '119273.jpg');
+(5, 1, 'Paracetamol', '17.99', 'Para.jpg'),
+(6, 2, 'Some text', '16.00', '373465.jpg'),
+(7, 2, 'Some text', '19.00', '373989.jpg'),
+(8, 2, 'Some text', '12.00', '374104.jpg'),
+(9, 2, 'Some text', '10.00', '4733.jpg'),
+(10, 2, 'Some text', '15.00', '834551.jpg'),
+(11, 3, 'Some text', '13.00', '908783.jpg'),
+(12, 3, 'Some text', '17.00', '835545.jpg'),
+(13, 3, 'Some text', '19.00', '119273.jpg');
 
 --
 -- Indexes for dumped tables
