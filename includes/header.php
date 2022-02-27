6af2e728bf868abb15a12257ee32783fda2149a3
Hallucion <D00234895@student.dkit.ie>
2022-02-27 14:02:39 +0000

    Sorted CSS

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <!-- the head section -->
 <head>
 <title>My PHP CRUD App</title>
-<link rel="stylesheet" type="text/css" href="main.css">
+<link rel="stylesheet" type="text/css" href="css/mystyle.css">
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>My PHP CRUD app</h1></header>
+<header><h1>Pharmacy Central</h1></header>
